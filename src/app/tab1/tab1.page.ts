54a401b40fabe644ae56a6b76b413dcead627949
Michel Vidailhet <michel.vidailhet@gmail.com>
2023-07-06 18:16:45 +0200

    Remove tab 3 Can search pokemon

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,19 +6,47 @@ import { Router } from '@angular/router';
 
 @Component({
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
   title = 'Tous les pokémons';
+  allPokemons?: Pokemon[];
   pokemons?: Pokemon[];
+  showSearchBar = false;
+  currentSearch = '';
 
   constructor(private apiService: ApiService, private router: Router) {
-
     this.apiService.getPokemons().subscribe((pokemons: Pokemon[]) => {
-      this.pokemons = pokemons.filter((pokemons: Pokemon, index: number) => index > 0 && index < 151);
+      this.allPokemons = pokemons.filter(
+        (pokemons: Pokemon, index: number) => index > 0 && index < 151
+      );
+      this.pokemons = this.allPokemons;
       console.log(this.pokemons);
     });
+  }
 
+  onSearchChange(event: Event) {
+    const customEvent = event as CustomEvent;
+    this.currentSearch = customEvent.detail.value;
+
+    if (this.currentSearch.length < 3) {
+      this.currentSearch = '';
+    };
+
+    this.filterPokemonsByCurrentSearch();
+  }
+
+  filterPokemonsByCurrentSearch() {
+    if (!this.allPokemons) return;
+    this.pokemons = this.allPokemons.filter((pokemon) => {
+      const transformedName = pokemon.name.fr.toLowerCase();
+      const withoutAccentsName = Utils.removeSpecialCharacters(transformedName);
+      return (
+        !this.currentSearch ||
+        transformedName.includes(this.currentSearch) ||
+        withoutAccentsName.includes(this.currentSearch)
+      );
+    });
   }
 
   goToPokemonPage(pokemonName: string) {
@@ -26,4 +54,7 @@ export class Tab1Page {
     this.router.navigate(['/pokemon', pokemonSlug]);
   }
 
+  toggleSearchBar() {
+    this.showSearchBar = !this.showSearchBar;
+  }
 }
