c854d5bad38df468b0bfa7fc5acbe7c046572cd7
Michel Vidailhet <michel.vidailhet@gmail.com>
2023-07-13 11:12:58 +0200

    Suppression des examples d'observables

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -3,7 +3,6 @@ import { ApiService } from '../../services/api.service';
 import { Pokemon } from '../../models/pokemons';
 import { Utils } from '../../utils/utils';
 import { Router } from '@angular/router';
-import { Observable, catchError, delay, interval, map, of, switchMap, tap } from 'rxjs';
 
 @Component({
   templateUrl: 'tab1.page.html',
@@ -20,7 +19,6 @@ export class Tab1Page implements OnDestroy {
     private apiService: ApiService,
     private router: Router,
   ) {
-    this.createAndSubsbscribeObservable();
     this.apiService.getPokemons().subscribe((pokemons: Pokemon[]) => {
       this.allPokemons = pokemons.filter(
         (pokemons: Pokemon, index: number) => index > 0 && index < 151
@@ -32,62 +30,6 @@ export class Tab1Page implements OnDestroy {
 
   ngOnDestroy(): void {
     throw new Error('Method not implemented.');
-  }
-
-  createAndSubsbscribeObservable() {
-    /*     const data$ = interval(1000);
-    data$.subscribe((res: number) => {
-      console.log(res);
-    }); */
-
-    /*     const data$ = new Subject();
-    data$.subscribe((value) => console.log(value));
-    data$.next('foo');
-
-    setTimeout(() => {
-      data$.next('foo2');
-    }, 3000); */
-
-    const data$ = new Observable((observer) => {
-      observer.next(1);
-      observer.next(2);
-      observer.error(new Error('Oups!'));
-      observer.next(3);
-      observer.next(4);
-      observer.next(6);
-      //observer.complete();
-    });
-
-  data$
-  .pipe(
-    catchError((error: any) => {
-      console.log(error);
-      return of(3, 4);
-    })
-  )
-  .subscribe({
-      next: (value) => console.log(value),
-      error: (err) => console.error(err),
-      complete: () => console.log('DONE!'),
-    });
-
-    //subscription.unsubscribe();
-
-/*     const data$ = new Observable<number>((observer) => {
-      observer.next(1);
-      observer.next(2);
-      observer.next(3);
-    }); */
-
-    //const data$ = interval(2000);
-
-/*     data$
-      .pipe(
-        switchMap((page: number) => {
-          return this.apiService2.getPokemons(20, page * 20);
-        }),
-      )
-      .subscribe((value) => console.log(value.results)); */
   }
 
   onSearchChange(event: Event) {
