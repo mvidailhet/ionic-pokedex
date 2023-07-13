61d0af583ce458b466d121f866d1d18370f7dea9
Michel Vidailhet <michel.vidailhet@gmail.com>
2023-07-13 12:02:43 +0200

    Renommage du nom de l'app. Ajout du fichier de conf des notifications pour Android

--- android/app/src/main/java/com/mitch/ion_pokedex/MainActivity.java
+++ android/app/src/main/java/com/mitch/ion_pokedex/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.mitch.ion_pokedex;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.mitch.ion_pokedex',
   appName: 'ionic-pokedex',
   webDir: 'www',
   server: {
