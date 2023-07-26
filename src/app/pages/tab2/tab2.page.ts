799511783415d69fd86e0f9c002706a9d0eadf46
Michel Vidailhet <michel.vidailhet@gmail.com>
2023-07-26 13:40:32 +0200

    Récupération des détails de chaque pokémon de la liste

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,6 +1,23 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { PAPokemons, PAPokemonSimple } from 'src/app/models/pa-pokemons';
+import {
+  EMPTY,
+  catchError,
+  concatMap,
+  delay,
+  from,
+  map,
+  mergeMap,
+  of,
+  switchMap,
+  tap,
+  zip,
+} from 'rxjs';
+import {
+  PAPokemon,
+  PAPokemons,
+  PAPokemonSimple,
+} from 'src/app/models/pa-pokemons';
 import { PokeapiService } from 'src/app/services/pokeapi.service';
 
 @Component({
@@ -10,15 +27,13 @@ import { PokeapiService } from 'src/app/services/pokeapi.service';
 })
 export class Tab2Page {
   title = 'Mes pokémons';
-  allPokemons?: PAPokemonSimple[];
-  nbPokemonsPerPage = 20;
+  allPokemons?: (PAPokemonSimple | PAPokemon)[];
+  currentPokemons?: (PAPokemonSimple | PAPokemon)[];
+  nbPokemonsPerPage = 5;
   nbTotalElements = 0;
   currentPage = 1;
 
-  constructor(
-    private pokeApiService: PokeapiService,
-    private router: Router
-  ) {
+  constructor(private pokeApiService: PokeapiService, private router: Router) {
     this.goToPage(this.currentPage);
   }
 
@@ -30,11 +45,41 @@ export class Tab2Page {
   }
 
   goToPage(page = 1) {
-    this.getPokemonsForPage(page).subscribe((pokemons: PAPokemons) => {
-      this.allPokemons = pokemons.results;
-      this.nbTotalElements = pokemons.count;
-      this.currentPage = page;
-    });
+    this.getPokemonsForPage(page)
+      .pipe(
+        tap((pokemons: PAPokemons) => {
+          this.allPokemons = pokemons.results;
+          this.currentPokemons = pokemons.results;
+          this.nbTotalElements = pokemons.count;
+          this.currentPage = page;
+        }),
+        delay(2000),
+        switchMap((pokemons: PAPokemons) => from(pokemons.results)),
+        mergeMap((pokemon: PAPokemonSimple, index: number) => {
+          if (index === 2) {
+            return zip(
+              of(index),
+              this.pokeApiService.getPokemon('lolilol').pipe(
+                delay(1000),
+                catchError((error) => {
+                  console.error(error);
+                  return EMPTY;
+                })
+              )
+            );
+          }
+          return zip(
+            of(index),
+            this.pokeApiService.getPokemon(pokemon.name).pipe(delay(1000))
+          );
+        }),
+      )
+      .subscribe(([index, pokemon]: [number, PAPokemon]) => {
+        if (!this.currentPokemons) return;
+        this.currentPokemons[index] = pokemon;
+        console.log(index);
+        console.log(pokemon);
+      });
   }
 
   goToPokemonPage(name: string) {
